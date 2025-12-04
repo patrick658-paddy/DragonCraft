21cf53a3f96932dd93043b1bac2999e24e40a330
Paddy Ji <zji1@ocdsb.ca>
2025-12-04 10:20:31 -0500

    qf

--- Dragon.java
+++ Dragon.java
@@ -2,35 +2,42 @@
 /**
  * Write a description of class Dragon here.
  *
- * @author (your name)
+ * @author (your strName)
  * @version (a version number or a date)
  */
 import java.util.ArrayList;
 public class Dragon
 {
     //initialize all instance variable of a dragon
-    String name;
-    short maxHp;
-    short health;
-    byte level;
-    short xp;
-    short armor;
+    String strName;
+    short shrMaxHp;
+    short shrHealth;
+    byte bytLevel;
+    short shrXp;
+    short shrArmor;
     ArrayList<Moves> moveList =new ArrayList<Moves>();
-    String statusEffect;
-    short sp;
+    String strStatusEffect;
+    short shrSp;
     
     public Dragon(String n,short m,byte l,short a,ArrayList<Moves>mo,short s )
     {
-        this. name=n;
-        this. maxHp=m;
-        this.health=this.maxHp;
-        this.level=l;
-        this.xp=0;
-        this.armor=a;
+        this. strName=n;
+        this. shrMaxHp=m;
+        this.shrHealth=this.shrMaxHp;
+        this.bytLevel=l;
+        this.shrXp=0;
+        this.shrArmor=a;
         this.moveList=mo;
-        this.statusEffect="NULL";
-        short sp=s;
+        this.strStatusEffect="NULL";
+        short shrSp=s;
     }
+    
+    
+    public void attack(Dragon target, Moves move)
+    {
+        
+    }
+    
     
     
 }
