af595ffe4b5e982228c602a4fe8ee2b92b106506
Paddy Ji <zji1@ocdsb.ca>
2025-12-04 09:52:46 -0500

    fdw

---
+++ Dragon.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Dragon here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dragon
+{
+    
+    private int x;
+    
+}
