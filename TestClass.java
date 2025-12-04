7163f5b84451e9ccdccda84005fcbaac92029bc9
Eric <ezhou2@ocdsb.ca>
2025-12-04 10:01:45 -0500

    Fireball move

--- TestClass.java
+++ TestClass.java
@@ -5,7 +5,13 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.ArrayList;
 public class TestClass
 {
-//kjyjkhkjhjk
+    //declare arrayList
+    ArrayList<Moves> AbilityLibrary = new ArrayList<>();
+    public void addMoves()
+    {
+        AbilityLibrary.add (new Moves ("Fireball", (byte)1, (short)500, false, (float)0.1, "Burn", (short)20, (short)0));
+    }
 }
