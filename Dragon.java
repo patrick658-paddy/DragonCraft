9cbe49a25efdde92e165d8efe7405c4523859700
Paddy Ji <zji1@ocdsb.ca>
2025-12-04 10:11:24 -0500

    grgre

--- Dragon.java
+++ Dragon.java
@@ -5,9 +5,32 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.ArrayList;
 public class Dragon
 {
+    //initialize all instance variable of a dragon
+    String name;
+    short maxHp;
+    short health;
+    byte level;
+    short xp;
+    short armor;
+    ArrayList<Moves> moveList =new ArrayList<Moves>();
+    String statusEffect;
+    short sp;
     
-    private int x;
+    public Dragon(String n,short m,byte l,short a,ArrayList<Moves>mo,short s )
+    {
+        this. name=n;
+        this. maxHp=m;
+        this.health=this.maxHp;
+        this.level=l;
+        this.xp=0;
+        this.armor=a;
+        this.moveList=mo;
+        this.statusEffect="NULL";
+        short sp=s;
+    }
+    
     
 }
