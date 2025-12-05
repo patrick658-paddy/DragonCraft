3765b9320f363b2e31ab3dc59edd55a82c82ec37
Eric <ezhou2@ocdsb.ca>
2025-12-05 11:06:19 -0500

    a

--- TestClass.java
+++ TestClass.java
@@ -14,6 +14,7 @@ public class TestClass
         Moves Fireball = new Moves ("Fireball", (byte)1, (short)500, false, (float)0.1, "Burn", (short)50, (short)0);
         Moves Splash = new Moves ("Splash", (byte)2, (short)250, false,(float)0, " ", (short)10, (short)0);
         Moves Freeze = new Moves ("Freeze", (byte)3, (short)200, false, (float)0.3, "Frozen", (short)40, (short)0);
+        Moves Earthquake = new Moves ("Earthquake", (byte)4, (short)350, false, (float)0, " ", (short)40, (short)0);
         Moves Bite = new Moves ("Bite", (byte)0, (short)400, false, (float)0, " ", (short)15, (short)0);
         Moves Heal = new Moves ("Heal", (byte)0, (short)200, true, (float)0, " ", (short)50, (short)0);
     }
