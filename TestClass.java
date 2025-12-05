8b49c16c4ed8c0351761b5c6043a2eca9a6a97ed
Eric <ezhou2@ocdsb.ca>
2025-12-05 11:28:11 -0500

    a

--- TestClass.java
+++ TestClass.java
@@ -13,13 +13,17 @@ public class TestClass
         Moves Fireball = new Moves ("Fireball", (byte)1, (short)450, false, (float)0.2, "Burn", (short)40, (short)0);
         Moves Fireclaw = new Moves ("Fireclaw", (byte)1, (short)500, false, (float)0, " ", (short)30, (short)0);
         Moves Firebreath = new Moves ("Firebreath", (byte)1, (short)600, false, (float)0.1, "Burn", (short)50, (short)0);
-        Moves Flame_Slash = new Moves ("Flame_Slash", (byte)1, (short)400, false, (float)0.2, "Burn", (short)20, (short)0);
+        Moves Flame_Slash = new Moves ("Flame Slash", (byte)1, (short)400, false, (float)0.2, "Burn", (short)20, (short)0);
         
         Moves Splash = new Moves ("Splash", (byte)2, (short)250, false,(float)0, " ", (short)10, (short)0);
-        Moves Water_Pump = new Moves ("Water_Pump", (byte)2, (short)400, false, (float)0, " ", (short)20, (short)0);
+        Moves Water_Pump = new Moves ("Water Pump", (byte)2, (short)400, false, (float)0, " ", (short)20, (short)0);
+        Moves Water_Shuriken = new Moves("Water Shuriken", (byte)2, (short)500, false, (float)0, "", (short)50, (short)0); 
         
         Moves Freeze = new Moves ("Freeze", (byte)3, (short)200, false, (float)0.3, "Frozen", (short)40, (short)0);
+        
+        
         Moves Earthquake = new Moves ("Earthquake", (byte)4, (short)350, false, (float)0, " ", (short)40, (short)0);
+        
         Moves Bite = new Moves ("Bite", (byte)0, (short)400, false, (float)0, " ", (short)15, (short)0);
         
         Moves Heal = new Moves ("Heal", (byte)0, (short)200, true, (float)0, " ", (short)50, (short)0);
