875e374cb9c33d1c68058d673559f3cb1a6c63ea
Eric <ezhou2@ocdsb.ca>
2025-12-08 09:41:25 -0500

    a

--- Player.java
+++ Player.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Player
 {
+    //declare variables
     String strName;
     ArrayList<Dragon> DragonList = new ArrayList<>();
     byte bytActiveDragon;
@@ -20,11 +21,39 @@ public class Player
         this.bytActiveDragon = ad;
     }
     
+    //method to pick active dragon
     public void pickDragon()
     {
-        System.out.println("Pick your Dragon");
+        byte bytDragon = 0;
+        String Dragon = "";
+        boolean doWhile = true;
+        
+        do
+        {
+            System.out.println("Do you want to change dragons? \nYes \nNo");
+            Dragon = new Scanner(System.in).nextLine().toUpperCase();
+            if(Dragon == "YES")
+            {
+                bytDragon++;
+                doWhile = false;
+                
+                System.out.println("Dragon selected " + DragonList.get(bytDragon));
+                
+            }
+            else if (Dragon == "NO")
+            {
+                doWhile = false;
+            }
+            else
+            {
+                System.out.println("Error Please input Yes or No");
+                doWhile = true;
+            }
+        }
+        while (doWhile == true);
         
     }
+    
     
     public void upgradeDragon(byte bytLevel)
     {
