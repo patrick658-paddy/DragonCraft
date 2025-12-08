a9cc209c507494a7df3718327f5cf4362cf7cf80
Paddy Ji <zji1@ocdsb.ca>
2025-12-08 09:44:33 -0500

    rgv

--- Player.java
+++ Player.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Player
 {
+    //declare variables
     String strName;
     ArrayList<Dragon> DragonList = new ArrayList<>();
     byte bytActiveDragon;
@@ -20,15 +21,64 @@ public class Player
         this.bytActiveDragon = ad;
     }
     
+    //method to pick active dragon
     public void pickDragon()
     {
-        System.out.println("Pick your Dragon");
+        byte bytDragon = 0;
+        String Dragon = "";
+        boolean doWhile = true;
+        
+        do
+        {
+            System.out.println("Do you want to change dragons? \nYes \nNo");
+            Dragon = new Scanner(System.in).nextLine().toUpperCase();
+            if(Dragon == "YES")
+            {
+                bytDragon++;
+                doWhile = false;
+                
+                System.out.println("Dragon selected " + DragonList.get(bytDragon));
+                
+            }
+            else if (Dragon == "NO")
+            {
+                doWhile = false;
+            }
+            else
+            {
+                System.out.println("Error Please input Yes or No");
+                doWhile = true;
+            }
+        }
+        while (doWhile == true);
         
         System.out.println();
     }
     
+    
     public void upgradeDragon(byte bytLevel)
     {
+        String Upgrade;
+        boolean doWhile = true;
+        System.out.println("Do you want to upgrade your active Dragon? \nYes \nNo");
         
+        Upgrade = new Scanner(System.in).nextLine().toUpperCase();
+        do
+            if(Upgrade == "YES")
+            {
+                doWhile = false;
+                
+            }
+            else if (Upgrade == "NO")
+            {
+                doWhile = false;
+            }
+            else
+            {
+                System.out.println("Error Please input Yes or No");
+                doWhile = true;
+
+            }
+        while (doWhile == true);
     }
 }
